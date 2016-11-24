d192d2eb8a5a9516d6e8249c8a163bb047ddebf1
Reidel Lazaro Rodriguez Torres <reidel.rodriguez@datazucar.cu>
2016-11-24 10:35:07 -0500

    url mejorada

--- enanancias/urls.py
+++ enanancias/urls.py
@@ -20,7 +20,7 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('mederoblog.urls', namespace="mederoblog")),
-    url(r'^weblog/', include('zinnia.urls')),
+    url(r'^mederosblog/', include('zinnia.urls')),
     url(r'^comments/', include('django_comments.urls')),
 
 ]
--- mederoblog/urls.py
+++ mederoblog/urls.py
@@ -4,7 +4,7 @@ from django.views.generic.base import RedirectView
 from django.contrib.sites.models import Site
 
 urlpatterns = [
-    url(r'^$', RedirectView.as_view(url="http://"+Site.objects.get_current().domain+"/weblog/", permanent=True), name='home'),
+    url(r'^$', RedirectView.as_view(url="http://"+Site.objects.get_current().domain+"/mederosblog/", permanent=True), name='home'),
     url(r'^humans.txt$', generic.TemplateView.as_view(template_name='mederoblog/humans.txt', content_type='text/plain'),
         name='humans'),
     url(r'^humans$', generic.TemplateView.as_view(template_name='mederoblog/humans.txt', content_type='text/plain'),
