8abe81df99d01b2c64f65e24788056de5d5e7351
Reidel Lazaro Rodriguez Torres <reidelon@gmail.com>
2016-12-16 15:46:30 -0500

    cambiado debug a False

--- enanancias/settings.py
+++ enanancias/settings.py
@@ -32,7 +32,7 @@ def get_env_variable(var_name):
 SECRET_KEY = get_env_variable('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
